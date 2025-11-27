ecfb38fca76e36b8426230091ff015e72ecb15db
Dawei Li <2819238408@qq.com>
2025-11-27 15:49:43 -0700

    update data

--- app.js
+++ app.js
@@ -336,7 +336,7 @@ const scheduleInitialFocus = () => {
 
 const init = async () => {
   try {
-    const response = await fetch("manning_tree_latest.json");
+    const response = await fetch("huan_liu_merged.json");
     state.data = await response.json();
   } catch (error) {
     console.error("Failed to load data", error);
